dbeac3f6b2b71e0ef7e36b07c54567733cc8fc51
h.batr <h.batr@net.estia.fr>
2021-11-05 16:43:23 +0100

    Project closed

---
+++ neighbors/src/main/java/fr/estia/net/batr/h/neighbors/NavigationListener.kt
@@ -0,0 +1,10 @@
+package fr.estia.net.batr.h.neighbors
+
+
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+
+interface NavigationListener {
+    fun showFragment(fragment: Fragment)
+    fun updateTitle(@StringRes title: Int)
+}
